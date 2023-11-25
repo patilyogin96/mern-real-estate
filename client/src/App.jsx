9d136d29354280ba7747804e9d02d26f50eb58ff
yogingten96 <yogin.patil@g10consultancy.com>
2023-11-25 11:12:17 +0530

    static header component implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import SignIn from './pages/SignIn'
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md ">
+      <div className="flex justify-between items-center max-w-6xl m-auto p-3 ">
+        <div>
+          <Link to="/">Yogin Estate</Link>
+        </div>
+        <div className="flex items-center relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="rounded-lg p-2 focus:outline-none w-72"
+          />
+          <span className="absolute right-2 ">
+            <FaSearch />
+          </span>
+        </div>
+        <div>
+          <ul className="flex justify-between gap-4 ">
+            <li>
+              {" "}
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="sign-up ">Sign up</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
