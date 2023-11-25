8176221633ae09fb44cdbd2680ba2e1394258591
yogingten96 <yogin.patil@g10consultancy.com>
2023-11-25 16:41:47 +0530

    network file for api call implemeted

---
+++ client/src/utils/network.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+export const base_url = "http://localhost:5000/api/";
+
+export const open_api = axios.create({
+  baseURL: base_url,
+  timeout: 300000,
+  headers: { "Content-Type": "application/json" },
+  validateStatus: (status) => status < 5000,
+  transformRequest: [
+    (data, headers) => {
+      return JSON.stringify(data);
+    },
+  ],
+});
+
+const setApiCall = () => {
+  let instance = axios.create({
+    baseURL: base_url,
+    timeout: 300000,
+    headers: { "Content-Type": "application/json" },
+    validateStatus: (status) => status < 500,
+    transformRequest: [
+      function (data, headers) {
+        return JSON.stringify(data);
+      },
+    ],
+  });
+
+  instance.interceptors.request.use((config) => {
+    const token = JSON.parse(localStorage.getItem("yogin-real-estate-token"));
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+    return config;
+  });
+
+  instance.interceptors.response.use((res) => {
+    if (res.status === 401) {
+      localStorage.removeItem("yogin-real-estate-token");
+      window.location.reload();
+      return res;
+    }
+    return res;
+  });
+
+  return instance;
+};
+
+export const token_api = setApiCall();
