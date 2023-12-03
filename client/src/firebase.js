7f8eb7bb54c5c1d96316bd4765d89e545c8dad65
yogingten96 <yogin.patil@g10consultancy.com>
2023-12-03 12:22:14 +0530

    firebase upload logic created-FOR trail purpose

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbfBPBgt0FvyqPOMDvdT0B67h92BJ1pe4",
+  authDomain: "mern-real-estate-e30e9.firebaseapp.com",
+  projectId: "mern-real-estate-e30e9",
+  storageBucket: "mern-real-estate-e30e9.appspot.com",
+  messagingSenderId: "773326857795",
+  appId: "1:773326857795:web:990d17604b5f28127cd6f6",
+};
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import prop1 from "../assets/realEstateassets/prop1.jpg";
@@ -6,8 +6,50 @@ import prop2 from "../assets/realEstateassets/prop2.jpg";
 import prop3 from "../assets/realEstateassets/prop3.jpg";
 import { Link } from "react-router-dom";
 import CardItem from "../components/CardItem/CardItem";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+import { app } from "../firebase";
 
 const Home = () => {
+  const [image, setImage] = useState({});
+  const [imageUrl, setImgUrl] = useState("");
+  const handleChange = (e) => {
+    console.log("IMage", e.target.files[0]);
+    setImage(e.target.files[0]);
+  };
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            console.log("DSDSDSA", url);
+            setImgUrl(url);
+            resolve(url);
+          });
+        }
+      );
+    });
+  };
+
+  useEffect(() => {
+    if (image) {
+      storeImage(image)
+        .then((url) => {})
+        .catch((err) => {});
+    }
+  }, [image]);
   return (
     <div>
       <div className="px-2 py-28 flex flex-col gap-2 max-w-6xl m-auto">
@@ -23,6 +65,15 @@ const Home = () => {
         <Link className="text-blue-700 text-lg hover:underline" to="/search">
           Lets Start now...
         </Link>
+
+        <div>
+          Upload Test
+          <div>
+            <input type="file" accept="image/*" onChange={handleChange} />
+          </div>
+        </div>
+
+        <img src={imageUrl || ""} alt="image" />
       </div>
 
       <Swiper navigation={true}>
