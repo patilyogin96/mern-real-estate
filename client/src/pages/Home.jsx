1510c0641b5f729c1b1e59a7fc19a8741a0739ef
yogingten96 <yogin.patil@g10consultancy.com>
2023-12-08 17:05:15 +0530

    api integration for proerty lisiting done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,50 +6,25 @@ import prop2 from "../assets/realEstateassets/prop2.jpg";
 import prop3 from "../assets/realEstateassets/prop3.jpg";
 import { Link } from "react-router-dom";
 import CardItem from "../components/CardItem/CardItem";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
-
+import { open_api } from "../utils/network";
 import { app } from "../firebase";
 
 const Home = () => {
   const [image, setImage] = useState({});
   const [imageUrl, setImgUrl] = useState("");
-  const handleChange = (e) => {
-    console.log("IMage", e.target.files[0]);
-    setImage(e.target.files[0]);
-  };
-  const storeImage = async (file) => {
-    return new Promise((resolve, reject) => {
-      const storage = getStorage(app);
-      const fileName = new Date().getTime() + file.name;
-      const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {},
-        (error) => {},
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-            console.log("DSDSDSA", url);
-            setImgUrl(url);
-            resolve(url);
-          });
-        }
-      );
-    });
+  const [listData, setListData] = useState([]);
+
+  const getPropertyListing = async () => {
+    const fetchList = await open_api.get(`v1/listing`);
+    console.log("PropData", fetchList);
+    setListData(fetchList?.data);
   };
 
   useEffect(() => {
-    if (image) {
-      storeImage(image)
-        .then((url) => {})
-        .catch((err) => {});
-    }
-  }, [image]);
+    getPropertyListing();
+  }, []);
+  console.log("ListData", listData);
+
   return (
     <div>
       <div className="px-2 py-28 flex flex-col gap-2 max-w-6xl m-auto">
@@ -65,13 +40,6 @@ const Home = () => {
         <Link className="text-blue-700 text-lg hover:underline" to="/search">
           Lets Start now...
         </Link>
-
-        <div>
-          Upload Test
-          <div>
-            <input type="file" accept="image/*" onChange={handleChange} />
-          </div>
-        </div>
 
         <img src={imageUrl || ""} alt="image" />
       </div>
@@ -101,13 +69,26 @@ const Home = () => {
       <div className="max-w-6xl mx-auto  py-20 px-2">
         <h1 className="font-semibold text-2xl md:text-4xl">Recent Offers</h1>
         <div className="flex flex-col gap-4 md:flex-row flex-wrap py-4">
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
+          {listData?.length > 0 &&
+            listData?.map((item) => {
+              const {
+                name,
+                description,
+                address,
+                discountPrice,
+                regularPrice,
+                imageUrls,
+              } = item;
+              return (
+                <CardItem
+                  title={name}
+                  description={description}
+                  address={address}
+                  price={regularPrice}
+                  image={imageUrls[0]}
+                />
+              );
+            })}
         </div>
       </div>
     </div>
