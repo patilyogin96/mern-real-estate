5c7c6a547c4542eb97cf542775bb675ddc75bfc9
yogingten96 <yogin.patil@g10consultancy.com>
2023-12-10 16:25:09 +0530

    query params logic added to get lising function

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 const Listing = require("../models/listing.model");
+const { errorHandler } = require("../utils/error");
 
 exports.addProperty = async (req, res, next) => {
   // console.log("REAChedListiongaa", req.body);
@@ -12,7 +13,53 @@ exports.addProperty = async (req, res, next) => {
 };
 exports.getAllListings = async (req, res, next) => {
   try {
-    const fetchList = await Listing.find({});
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const q = req.query.q || "dattani";
+
+    const fetchList = await Listing.find({
+      name: { $regex: q, $options: "i" },
+      furnished,
+      offer,
+      type,
+      parking,
+    });
+    console.log("Furnished", furnished, offer, parking, type, q, fetchList);
     return res.status(200).json(fetchList);
-  } catch (error) {}
+  } catch (error) {
+    next(errorHandler(error));
+  }
+};
+
+exports.getSingleProperty = async (req, res, next) => {
+  try {
+    const getProperty = await Listing.findById(req.params.id);
+
+    if (!getProperty) {
+      return next(errorHandler(404, "Property not found"));
+    }
+
+    res.status(200).json(getProperty);
+  } catch (error) {
+    next(errorHandler(error));
+  }
 };
--- api/routes/api/v1/listing.route.js
+++ api/routes/api/v1/listing.route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getAllListings,
   addProperty,
+  getSingleProperty
 } = require("../../../controllers/listing.controller");
 
 router.get("/", getAllListings);
+router.get("/:id", getSingleProperty);
 
 // only admins can add property
 router.post("/add", addProperty);
