3eff84ba5e738e319fe56e7143c78b781d174166
yogingten96 <yogin.patil@g10consultancy.com>
2023-12-10 12:18:41 +0530

    google-auth completed from front-end

--- client/src/components/GoogleOauth/GoogleOauth.jsx
+++ client/src/components/GoogleOauth/GoogleOauth.jsx
@@ -2,20 +2,39 @@ import React from "react";
 import { app } from "../../firebase";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { useDispatch } from "react-redux";
+import { open_api } from "../../utils/network";
+import { useNavigate } from "react-router-dom";
+import { signInFail, signInSuccess } from "../../redux/user/userSlice";
 
 const GoogleOauth = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleGoogleAuthentication = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-      console.log("GOOGLEAUTH", result);
-      //   const apiAuthData = {
-      //     name: result?.user?.displayName,
-      //     email: result?.user?.email,
-      //     photo: result?.user?.photoURL,
-      //   };
-    } catch (error) {}
+
+      const apiAuthData = {
+        name: result?.user?.displayName,
+        email: result?.user?.email,
+        photo: result?.user?.photoURL,
+      };
+
+      const signInResponse = await open_api.post(
+        `v1/auth/google-login`,
+        apiAuthData
+      );
+      console.log("GOOGLEAUTH", signInResponse);
+      if (signInResponse?.status === 200) {
+        dispatch(signInSuccess(signInResponse?.data));
+        navigate("/");
+        return;
+      }
+      dispatch(signInFail(signInResponse?.data?.message));
+    } catch (error) {
+      dispatch(signInFail(error?.message));
+    }
   };
 
   return (
